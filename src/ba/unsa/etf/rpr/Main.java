f835dd1fa2b7bca3eab9ad944a859a77e870363a
mborovac1 <mborovac1@etf.unsa.ba>
2020-10-30 14:51:22 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sat s = new Sat(15, 30, 45);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.pomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0, 0, 0);
+        s.Ispisi();
+    }
+}
